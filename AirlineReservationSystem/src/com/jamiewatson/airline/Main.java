7a6f35df502232cdfa9842e06268cf8a4aa7cd3b
Jamie Watson <50807261+JamieWatson89@users.noreply.github.com>
2020-08-22 13:42:22 +0200

    Add files via upload

---
+++ AirlineReservationSystem/src/com/jamiewatson/airline/Main.java
@@ -0,0 +1,12 @@
+package com.jamiewatson.airline;
+
+public class Main {
+    public static void main(String[] args) {
+        Airline airline = new Airline();
+
+
+        airline.assignSeat("first");
+
+
+    }
+}
